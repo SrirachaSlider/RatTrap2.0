e201714358cd1319b635a422f0e971f5eae18f7e
FireCon <89361575+Ruben-Jim@users.noreply.github.com>
2025-09-04 23:21:04 -0700

    Initial start

--- src/main.cpp
+++ src/main.cpp
@@ -7,29 +7,6 @@ int distance;
 const int trigPin = 7;
 const int echoPin = 6;
 
-void setup()
-{
-  // put your setup code here, to run once:
+void setup() {}
 
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  Serial.begin(115200);
-}
-
-void loop()
-{
-  // put your main code here, to run repeatedly:
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, LOW);
-
-  duration = pulseIn(echoPin, HIGH);
-
-  distance = (duration / 29) / 2; // cm
-
-  Serial.print("Distance: ");
-  Serial.println(distance);
-}
+void loop() {}
